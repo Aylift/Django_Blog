cba827f0b2080ed939ace18845af25774a9eba8f
Aylift <adam.martka@gmail.com>
2025-02-06 17:05:27 +0100

    [ADD] added html files and rendering

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+POSTS = [
+    {
+        'author': 'Aylift',
+        'title': 'To be or not to be',
+        'content': 'First post',
+        'date': '3 Feb 2025'
+    },
+    {
+        'author': 'Aylift',
+        'title': 'Lorem ipsum',
+        'content': 'Second post',
+        'date': '4 Feb 2025'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Hello on my blog!</h1>')
+    return render(request, 'blog/home.html', {'title': 'Home', 'posts': POSTS})
 
 def about(request):
-    return HttpResponse('<h1>About Us</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})

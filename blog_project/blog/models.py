1b7747b027ec115ccc4126ade8e4a8e8d98faebf
Aylift <adam.martka@gmail.com>
2025-02-11 12:22:12 +0100

    [ADD] added Article model, adjusted views and html, added Article to admin panel

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Call 07.02.2025
+# class Example(models.Model):
+#     id = models.IntegerField(primary_key=True)
+#     name = models.CharField(max_length=100, unique=True)
+#
+#
+# class Specializations(models.TextChoices):
+#     PYTHON = "Python"
+#     JAVA = "Java"
+#     DATA = "Data"
+#
+#
+# class Mentor(models.Model):
+#     name = models.CharField(max_length=70)
+#     specialization = models.CharField(max_length=30, choices=Specializations.choices)
+#
+#
+# class Student(models.Model):
+#     name = models.CharField(max_length=70)
+#     bio = models.TextField()
+#     mentor = models.ForeignKey(to=Mentor, on_delete=models.SET_NULL, null=True)
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Article
 
 
-POSTS = [
-    {
-        'author': 'Aylift',
-        'title': 'To be or not to be',
-        'content': 'First post',
-        'date': '3 Feb 2025'
-    },
-    {
-        'author': 'Aylift',
-        'title': 'Lorem ipsum',
-        'content': 'Second post',
-        'date': '4 Feb 2025'
-    }
-]
+# POSTS = [
+#     {
+#         'author': 'Aylift',
+#         'title': 'To be or not to be',
+#         'content': 'First post',
+#         'date': '3 Feb 2025'
+#     },
+#     {
+#         'author': 'Aylift',
+#         'title': 'Lorem ipsum',
+#         'content': 'Second post',
+#         'date': '4 Feb 2025'
+#     }
+# ]
 
 def home(request):
-    return render(request, 'blog/home.html', {'title': 'Home', 'posts': POSTS})
+    # return render(request, 'blog/home.html', {'title': 'Home', 'posts': POSTS})
+    return render(request, 'blog/home.html', {'title': 'Home', 'posts': Article.objects.all()})
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})

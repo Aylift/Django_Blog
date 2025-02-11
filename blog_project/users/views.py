55fa4d0d14ffbb0be505422d57425ff2908f0d4f
Aylift <adam.martka@gmail.com>
2025-02-12 00:23:20 +0100

    [UPDATE] updated templates

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account's been created for {username}")
-            return redirect('blog-home')
+            messages.success(request, f"Dear {username}, you have been successfully signed up!")
+            return redirect('login')
     else:
         form = UserRegisterForm()
 

5e86064843c409b11941bf594555677ed742361e
Aylift <adam.martka@gmail.com>
2025-02-13 15:21:04 +0100

    [ADD] added updating and deletion functionality

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Article
 
 
@@ -49,3 +49,27 @@ class ArticleCreateView(LoginRequiredMixin ,CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Article
+    template_name = 'blog/article_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        article = self.get_object()
+        return self.request.user == article.author
+
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Article
+    template_name = 'blog/delete_confirm.html'
+    success_url = '/'
+
+    def test_func(self):
+        article = self.get_object()
+        return self.request.user == article.author

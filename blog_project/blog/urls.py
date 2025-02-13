d8e12b2d0e062fdf2d8320bba434ea31b2abc281
Aylift <adam.martka@gmail.com>
2025-02-13 13:57:35 +0100

    [ADD][UPDATE] added template for detil view of post, updated code for detailed view

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail')
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Article
 
 
@@ -31,3 +32,8 @@ class ArticleListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article.html'

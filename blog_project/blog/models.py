d2dc9bb0ca2e4d15cb0e97d0e40525dea1bc6f7a
Aylift <adam.martka@gmail.com>
2025-02-20 16:23:12 +0100

    [RM][UPDATE] removed comments, updated .gitignore

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -4,28 +4,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-# Call 07.02.2025
-# class Example(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=100, unique=True)
-#
-#
-# class Specializations(models.TextChoices):
-#     PYTHON = "Python"
-#     JAVA = "Java"
-#     DATA = "Data"
-#
-#
-# class Mentor(models.Model):
-#     name = models.CharField(max_length=70)
-#     specialization = models.CharField(max_length=30, choices=Specializations.choices)
-#
-#
-# class Student(models.Model):
-#     name = models.CharField(max_length=70)
-#     bio = models.TextField()
-#     mentor = models.ForeignKey(to=Mentor, on_delete=models.SET_NULL, null=True)
-
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
